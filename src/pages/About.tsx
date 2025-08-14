97c314e55c583a06ca130369316ad55eb6a5b162
SJ91Codes <99019986+SJ91Codes@users.noreply.github.com>
2025-08-14 10:34:45 +0100

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -91,7 +91,7 @@ const About: React.FC = () => {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center mb-16">
               <h2 className="text-4xl md:text-5xl font-bold font-rajdhani text-off-white mb-6">
-                Who I Work With
+                Who We Work With
               </h2>
             </div>
 
