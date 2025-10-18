a8f2875d20f284dc9f453db98a59e586742660ea
SJ91Codes <99019986+SJ91Codes@users.noreply.github.com>
2025-10-18 02:37:32 +0100

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,7 +59,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-charcoal/95 navbar-blur border-b border-slate/20">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-black navbar-blur border-b border-slate/20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <Link to="/" className="flex items-center">
